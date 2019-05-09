d4b1f9cc8a2de06183367a2f0d52ab20043a9267
Brian E Simon <bsssshhhhhhh@gmail.com>
2019-05-09 18:18:48 -0400

    Add support for editing cells

---
+++ src/edit-history.js
@@ -0,0 +1,111 @@
+const DeepDiff = require('deep-diff');
+const applyChange = DeepDiff.applyChange;
+const diff = DeepDiff.diff;
+/**
+ * Edit history tracker for a javascript object using deep-diff to generate and apply patches
+ */
+export default class EditHistory {
+  /**
+   * Instantiates an instance of EditHistory
+   * @param {Object} obj The object or array to track
+   */
+  constructor(obj) {
+    this.obj = JSON.parse(JSON.stringify(obj));
+    
+    // initialize arrays for forwards and backwards patches
+    this.forward = [];
+    this.backward = [];
+  }
+
+  /**
+   * Clears all forward and backward patches
+   */
+  clear() {
+    this.forward = [];
+    this.backward = [];
+  }
+
+  /**
+   * Records a change to an object
+   * @param {Object} newObj The new object
+   */
+  recordChange(newObj) {
+    const patch = {
+      redo: diff(this.obj, newObj),
+      undo: diff(newObj, this.obj)
+    };
+
+    this.obj = JSON.parse(JSON.stringify(newObj));
+
+    if (!patch.redo || !patch.undo) {
+      console.warn('Objects could not be diffed');
+    }
+    else {
+      this.backward.push(patch);
+    }
+  }
+
+  /**
+   * Applies the most recent undo patch and returns the new object
+   * @returns {Object} The tracked object
+   */
+  undo() {
+    if (this.backward.length === 0) {
+      return null;
+    }
+
+    // grab the most recent backwards patch
+    const patch = this.backward.pop();
+
+    // applyChange doesn't accept arrays, only its members
+    patch.undo.forEach(x => applyChange(this.obj, x));
+
+    // put the patch into the forward queue
+    this.forward.push(patch);
+
+    return JSON.parse(JSON.stringify(this.obj));
+  }
+
+  /**
+   * Applies the most recent redo patch and returns the new object
+   * @returns {Object} The tracked object
+   */
+  redo() {
+    if (this.forward.length === 0) {
+      return null;
+    }
+
+    // grab the most recent forwards patch
+    const patch = this.forward.pop();
+
+    // applyChange doesn't accept arrays, only its members
+    patch.redo.forEach(x => applyChange(this.obj, x));
+
+    // put the patch into the backward queue
+    this.backward.push(patch);
+
+    return JSON.parse(JSON.stringify(this.obj));
+  }
+
+  /**
+   * Applies all the undo patches in the queue and returns the new object
+   * @returns {Object} The tracked object
+   */
+  undoAll() {
+    while (this.backward.length > 0) {
+      this.undo();
+    }
+
+    return this.obj;
+  }
+
+  /**
+   * Applies all the redo patches in the queue and returns the new object
+   * @returns {Object} The tracked object
+   */
+  redoAll() {
+    while (this.forward.length > 0) {
+      this.redo();
+    }
+  }
+}
