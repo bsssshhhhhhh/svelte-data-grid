c202d3595334bdc87e57876f60792eb9dd6315f5
thecodejack <aadisrikanth@gmail.com>
2020-01-04 09:05:44 +0530

    initial changes for svelte 3 upgrade

--- src/textbox-cell.svelte
+++ src/textbox-cell.svelte
@@ -1,94 +1,98 @@
-<div class="textbox-cell">
-  <input type="text" on:input="onInput(event)" on:focus="onFocus(event)" on:blur="onBlur(event)" ref:textbox>
-</div>
-
-<style>
-.textbox-cell {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  background: white;
-  z-index: 3;
-}
-
-input {
-  height: 100%;
-  width: 100%;
-  border: 0;
-  margin: 0;
-  padding: 0 5px;
-  box-sizing: border-box;
-}
-
-input:active,
-input:focus {
-  border: 1px solid lime;
-}
-</style>
-
 <script>
-import debounce from 'debounce';
-
-const INPUT_DEBOUNCE_INTERVAL = 400;
-
-export default {
-  onstate({ changed, current }) {
-    if ((changed.column || changed.row)) {
+  import { afterUpdate, beforeUpdate, createEventDispatcher } from "svelte";
+  import debounce from "debounce";
+  const INPUT_DEBOUNCE_INTERVAL = 400;
+  const dispatch = createEventDispatcher();
+  export let textbox = null;
+  export let column;
+  export let rowNumber;
+  export let row;
+  let prevColumn;
+  let prevRow;
+  // [svelte-upgrade warning]
+  // beforeUpdate and afterUpdate handlers behave
+  // differently to their v2 counterparts
+  beforeUpdate(() => {
+    if (prevColumn !== column || prevRow !== row) {
       const updateTextbox = () => {
-        this.refs.textbox.value = current.row.data[current.column.dataName];
-      }
-      if (this.refs.textbox) {
+        if (textbox) textbox.value = row.data[column.dataName];
+      };
+      if (textbox) {
         updateTextbox();
       } else {
         setTimeout(updateTextbox, 0);
       }
-      
+      prevColumn = column;
     }
-  },
-  onupdate({ changed, current }) {
+  });
+  // [svelte-upgrade warning]
+  // beforeUpdate and afterUpdate handlers behave
+  // differently to their v2 counterparts
+  afterUpdate(() => {
     /* Since data-grid isn't using a keyed each block to display the rows, we need to update
-       the focus as the grid scrolls. When this cell component receives a new row, check if the column's active row
-       is this row, and focus or blur if necessary */
-    if (changed.row) {
-      if (current.column.activeRow && current.column.activeRow === current.rowNumber && this.refs.textbox) {
-        this.refs.textbox.focus();
-      } else if (this.refs.textbox === document.activeElement) {
-        this.refs.textbox.blur();
+      the focus as the grid scrolls. When this cell component receives a new row, check if the column's active row
+      is this row, and focus or blur if necessary */
+    if (prevRow !== row) {
+      if (column.activeRow && column.activeRow === rowNumber && textbox) {
+        textbox.focus();
+      } else if (textbox === document.activeElement) {
+        textbox.blur();
       }
+      prevRow = row;
     }
-  },
-  methods: {
-    onFocus(event) {
-      const { column, rowNumber } = this.get();
-      column.activeRow = rowNumber;
-    },
-    onBlur(event) {
-      const { column } = this.get();
-
-      // if blur event was user-initiated and not initiated by the blur call above,
-      // remove the activeRow property
-      if (event.sourceCapabilities) {
-        delete column.activeRow;
-      }
-    },
-
-    /**
-     * Debounce the oninput handler so that there isn't edit history for every keystroke
-     */
-    onInput: debounce(function (event) {
-      const value = this.refs.textbox.value;
-      const { row, column, rowNumber } = this.get();
-      setTimeout(() => {
-        this.fire('valueupdate', { 
-          row, 
-          column, 
-          value, 
-          rowNumber 
-        });
-      });
-      
-    }, INPUT_DEBOUNCE_INTERVAL)
+  });
+  // [svelte-upgrade suggestion]
+  // review these functions and remove unnecessary 'export' keywords
+  export function onFocus(event) {
+    column.activeRow = rowNumber;
   }
-}
+  export function onBlur(event) {
+    // if blur event was user-initiated and not initiated by the blur call above,
+    // remove the activeRow property
+    if (event.sourceCapabilities) {
+      delete column.activeRow;
+    }
+  }
+  export function onInput(event) {
+    const value = textbox.value;
+    setTimeout(() => {
+      dispatch("valueupdate", {
+        row,
+        column,
+        value,
+        rowNumber
+      });
+    }, 0);
+  }
 </script>
 
+<style>
+  .textbox-cell {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    background: white;
+    z-index: 3;
+  }
+  input {
+    height: 100%;
+    width: 100%;
+    border: 0;
+    margin: 0;
+    padding: 0 5px;
+    box-sizing: border-box;
+  }
+  input:active,
+  input:focus {
+    border: 1px solid lime;
+  }
+</style>
+
+<div class="textbox-cell">
+  <input
+    type="text"
+    on:input={onInput}
+    on:focus={onFocus}
+    on:blur={onBlur}
+    bind:this={textbox} />
+</div>
